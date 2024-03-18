8539ab5b2aa68b25693e7dfed9f0b873b680fc50
Ranujan Kathir <ranu@MacBook-Air-Ranu.local>
2024-03-18 14:27:03 +0100

    modif

--- arithmetic.py
+++ arithmetic.py
@@ -1,3 +1,5 @@
+import random
+
 # Exercise : arithmetic
 def product_list(L): 
     lon = len(L)
@@ -11,3 +13,24 @@ def product_list(L):
     return False
 
 print(product_list([1,2,3,4,5,5,6,7,3,10,3,5]))
+
+def product_tab(T):
+    nb_row = len(T)
+    nb_col = len(T[0])
+    if len(T) == 0:
+        return False
+    for i in range(nb_col):
+        for j in range(nb_row):
+            for k in range(j+1, nb_row):
+                for l in range(k+1, nb_row):
+                    if T[i][j]*T[i][k]*T[i][l] == 30:
+                        return True
+    return False
+
+# Define the size of the list 
+size_list = 10
+plage_entiers = (1, 100)  # Natural number between 1 and 100
+
+# Generate random value
+liste_aleatoire = [random.randint(plage_entiers[0], plage_entiers[1]) for _ in range(size_list)]
+print(product_tab(liste_aleatoire))
